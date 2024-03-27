474f25335618c6a8bd4413b4632b31ea7297bee7
edugmenes <edugmenes1@hotmail.com>
2024-03-26 22:41:35 -0300

    Details: - Foram atualizadas as tecnologias de comunicação http AJAX para Fecth API para os arquivos: "ajaxCadastroRecepcionista"; "ajaxAg";

--- client/public/js/usuario/agendamento/ajaxAg.js
+++ client/public/js/usuario/agendamento/ajaxAg.js
@@ -1,162 +1,110 @@
 // import {verificacao} from '/js/usuario/agendamento/funcaoAg.js'
-$(document).ready(function(){
-    
-    idUser = localStorage.getItem('idUser');
-    
-    $.ajax({
-        url:'/user/agendamento/dadosPsico',
-        type: 'GET'
-    })
-    .done(function(data){
-        console.log('Psicologos, agenda e horários:  ', data);
-        verificacao(data.psicologos, data.agenda, data.horarios);
-        generateCalendar(data.agenda, data.horarios);
-        selecionarPsico(data.psicologos);
-        
-        
-        
-    })
-    .fail(function(xhr, status, errorThrowne){
-        console.log('status: ', status);
-        console.log('error: ', errorThrowne);
-        console.log(xhr);
-    })
-    .always(function(){
-        console.log('Requisição Finalizada');
-    })
 
-    // Variável global:
-    var hora;
-    var idUser;
-    var button;
-    var msgWpp;
+document.addEventListener('DOMContentLoaded', function () {
 
-    $('#containerAgenda').on('click', '.buttonHora', function(){
-        button = $(this);
-        hora = $(this).text();
-        console.log('Id do Usuario: ', idUser);
-        $('#containerConfirmacao').css({
-            'display':'block'
+    // Variáveis globais:
+    let hora;
+    let button;
+    let msgWpp;
+    let idUser = localStorage.getItem('idUser');
+
+    fetch('/user/agendamento/dadosPsico')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response falhou.');
+            }
+            return response.json();
         })
-        $('#containerAgenda').css({
-            'display':'none'
+        .then(data => {
+            console.log('Psicologos, agenda e horários:  ', data);
+            verificacao(data.psicologos, data.agenda, data.horarios);
+            generateCalendar(data.agenda, data.horarios);
+            selecionarPsico(data.psicologos);
         })
+        .catch(error => {
+            console.error('Erro no fetch de dados:', error);
+        })
+        .finally(() => {
+            console.log('Requisição Finalizada');
+        });
 
-    })
-    
-    $('#containerConfirmacao').on('click', 'button', function(){ // Inicio do AJAX para confirmar agendamento.
-        console.log('Id do usuário: ', idUser);
-        console.log('button: ', hora);
-        msgWpp = $('#msgWpp')
+    document.getElementById('containerAgenda').addEventListener('click', function (event) {
+        if (event.target.classList.contains('buttonHora')) {
+            button = event.target;
+            hora = event.target.textContent;
+            console.log('Id do Usuario: ', idUser);
+            document.getElementById('containerConfirmacao').style.display = 'block';
+            document.getElementById('containerAgenda').style.display = 'none';
+        }
+    });
 
-        if(msgWpp.prop('checked')){ // Método .prop('checked') retorna true se o checkbox estiver marcado e false caso contrário.
+    document.getElementById('containerConfirmacao').addEventListener('click', function (event) {
+        if (event.target.tagName === 'BUTTON') {
+            console.log('Id do usuário: ', idUser);
+            console.log('button: ', hora);
+            msgWpp = document.getElementById('msgWpp').checked;
 
-            button.css({
-                'display':'none'
-            })
-            var data = {
+            button.style.display = 'none';
+
+            let data = {
                 idUser: idUser,
                 hora: hora
-            }
+            };
 
-            console.log('Objeto data: ', data)
-            $.ajax({
-                url: '/user/inserirHorario/wpp',
-                type: 'POST',
-                data: data
+            console.log('Objeto data: ', data);
+
+            let url = msgWpp ? '/user/inserirHorario/wpp' : '/user/inserirHorario';
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
             })
-            .done(function(resultServer){
-                console.log(resultServer);
-                $('#containerConfirmacao button').css({
-                    'display': 'none'
-                });
-                $('#containerMsgWpp').animate({
-                    'opacity':'0'
-                },1000, function(){
-                    $('#containerConfirmacao h5').animate({
-                        'opacity': 'toggle',
-                        'height': 'toggle'
-                    }, 1000, function() {
-                        $('#containerConfirmacao').animate({
-                            'opacity': '0'
-                        }, 1000, function() {
-                            $('#containerConfirmacao').css({
-                                'display': 'none'
-                            });
-                            $('#containerAgenda').animate({
-                                'height': 'toggle',
-                                'opacity': 'toggle'
-                            }, 1000, function(){
-                                window.location.reload();
-                            });
-                        });
-                    });
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response falhou.');
+                    }
+                    return response.json();
                 })
-                window.location.href = '/user/principal'
-            })
-            .fail(function(status, xhr, errorThrown){
-                console.log('status: ', status);
-                console.log(xhr);
-                console.log('error: ', errorThrown);
-            })
-            .always(function(){
-                console.log('Requisição finalizada');
-            })
-        }
-        else{
-            button.css({
-                'display':'none'
-            })
-            var data = {
-                idUser: idUser,
-                hora: hora
-            }
-
-            console.log('Objeto data: ', data)
-            $.ajax({
-                url: '/user/inserirHorario',
-                type: 'POST',
-                data: data
-            })
-            .done(function(resultServer){
-                console.log(resultServer);
-                $('#containerConfirmacao button').css({
-                    'display': 'none'
-                });
-                $('#containerConfirmacao h5').animate({
-                    'opacity': 'toggle',
-                    'height': 'toggle'
-                }, 1000, function() {
-                    $('#containerConfirmacao').animate({
-                        'opacity': '0'
-                    }, 1000, function() {
-                        $('#containerConfirmacao').css({
-                            'display': 'none'
-                        });
-                        $('#containerAgenda').animate({
-                            'height': 'toggle',
-                            'opacity': 'toggle'
-                        }, 1000, function(){
-                            window.location.reload();
-
+                .then(resultServer => {
+                    console.log(resultServer);
+                    document.querySelectorAll('#containerConfirmacao button').forEach(button => {
+                        button.style.display = 'none';
+                    });
+                    document.getElementById('containerMsgWpp').animate({
+                        opacity: '0'
+                    }, 1000, function () {
+                        document.getElementById('containerConfirmacao').querySelector('h5').animate({
+                            opacity: 'toggle',
+                            height: 'toggle'
+                        }, 1000, function () {
+                            document.getElementById('containerConfirmacao').animate({
+                                opacity: '0'
+                            }, 1000, function () {
+                                document.getElementById('containerConfirmacao').style.display = 'none';
+                                document.getElementById('containerAgenda').animate({
+                                    height: 'toggle',
+                                    opacity: 'toggle'
+                                }, 1000, function () {
+                                    window.location.reload();
+                                });
+                            });
                         });
                     });
+                    window.location.href = '/user/principal';
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                })
+                .finally(() => {
+                    console.log('Requisição finalizada!');
                 });
-            })
-            .fail(function(status, xhr, errorThrown){
-                console.log('status: ', status);
-                console.log(xhr);
-                console.log('error: ', errorThrown);
-            })
-            .always(function(){
-                console.log('Requisição finalizada!');
-            })
         }
-    }) // Fim do AJAX para confirmar agendamento!
+    });
 
-    $('#voltar').on('click', function(){
+    document.getElementById('voltar').addEventListener('click', function () {
         window.location.href = '/user/principal';
-    })
-
-
-})
+    });
+});
--- client/public/js/usuario/agendamento/funcaoAg.js
+++ client/public/js/usuario/agendamento/funcaoAg.js
@@ -1,20 +1,18 @@
-
-
-
- function verificacao(psicologos, agenda, horarios){
-    if(!agenda){
-        $('#psicoDados').append(`<h3>Sem agenda para consultas!</h3>`);
-    }
+function verificacao(psicologos, agenda, horarios) {
+  if (!agenda) {
+    $('#psicoDados').append(`<h3>Sem agenda para consultas!</h3>`);
+  }
 }
 
-
-
-function generateCalendar(agenda, horarios){
+function generateCalendar(agenda, horarios) {
   console.log('agenda: ', agenda)
+
   const calendar = document.getElementById('calendar');
   const currentDate = new Date();
   const month = currentDate.getMonth();
-  console.log('month: ' ,month)
+
+  console.log('month: ', month)
+
   var selectMonth = document.querySelector('.selectMonth');
   selectMonth.value = month;
   const year = currentDate.getFullYear();
@@ -22,135 +20,146 @@ function generateCalendar(agenda, horarios){
   const firstDayOfMonth = new Date(year, month, 1);
   console.log('firstDayOfMonth: ', firstDayOfMonth);
 
-  const lastDayfMonth = new Date(year, month+1, 0);
+  const lastDayfMonth = new Date(year, month + 1, 0);
   console.log('lastDayfMonth: ', lastDayfMonth);
-  
-  //Primeiro dia Da Semana
+
+  //Primeiro dia da semana:
   const firstDayOfWeek = firstDayOfMonth.getDay();
   console.log('firstDayOfWeek: ', firstDayOfWeek);
   const totalDays = lastDayfMonth.getDate();
   console.log('totalDays: ', totalDays);
 
-  for(let i = 0; i< firstDayOfWeek; i++){
+  for (let i = 0; i < firstDayOfWeek; i++) {
     let blankDay = document.createElement('div');
     // blankDay.classList.add('borda');
-    
+
     calendar.appendChild(blankDay);
   }
-  for(let day =0; day<= totalDays; day++){
+
+  for (let day = 0; day <= totalDays; day++) {
     let daySquares = document.createElement('div')
+
     daySquares.className = 'calendar-day';
     // daySquares.classList.add('borda');
     daySquares.textContent = day;
     daySquares.id = `day-${day}`
-    for(let j = 0; j<agenda.length; j++ ){
+
+    for (let j = 0; j < agenda.length; j++) {
+
       var agendaDate = new Date(agenda[j].data);
       var agendaDay = agendaDate.getDate();
+
       console.log('AgendaDate: ', agendaDate);
       console.log('AgendaDay: ', agendaDay);
 
-      if(agendaDay == day){
-        
-      //lista de seleção de horários
+      if (agendaDay == day) {
+
+        //lista de seleção de horários
         var selectHours = document.createElement('select')
         selectHours.className = 'select-hours'
+
         var option = document.createElement('option');
         option.textContent = 'Horários Disponíveis'
+
         // Defina os atributos disabled e selected para desabilitar e selecionar a opção inicial
         option.disabled = true;
         option.selected = true;
         selectHours.appendChild(option);
-             
-        for(let x = 0; x<horarios.length; x++){
-          if(horarios[x].idAgenda == agenda[j].idAgenda){
+
+        for (let x = 0; x < horarios.length; x++) {
+
+          if (horarios[x].idAgenda == agenda[j].idAgenda) {
             var buttonOption = document.createElement('option');
+
             buttonOption.className = 'option-hours'
             buttonOption.textContent = horarios[x].hora;
             selectHours.append(buttonOption);
-            daySquares.append(selectHours); 
-          
+            daySquares.append(selectHours);
+
           }
         }
       }
     }
-  
     calendar.appendChild(daySquares);
   }
-
 }
 
-function showAddTaskModal(){
-  document.getElementById('addTaskModal').style.display ='block'
+function showAddTaskModal() {
+  document.getElementById('addTaskModal').style.display = 'block'
   document.getElementById('addTaskModal').classList.add('borda');
 }
 
-function closeAddTaskModal(){
-  document.getElementById('addTaskModal').style.display ='none'
+function closeAddTaskModal() {
+  document.getElementById('addTaskModal').style.display = 'none'
 }
 
-function deleteTask(taskElement){
-  if(confirm("Are you sure you Want to delete this task?")){
+function deleteTask(taskElement) {
+  if (confirm("Are you sure you Want to delete this task?")) {
     taskElement.parentNode.removeChild(taskElement)
   }
 }
 
-function editTask(taskElement){
+function editTask(taskElement) {
   const newTaskDesc = prompt('Edit Your task: ', taskElement.textContent)
-  
-  if(newTaskDesc !== null && newTaskDesc.trim() !== ''){
+
+  if (newTaskDesc !== null && newTaskDesc.trim() !== '') {
     taskElement.textContent = newTaskDesc
   }
 }
 
-function addTask(){
+function addTask() {
   const taskDate = new Date(document.getElementById('task-date').value)
-  const taskDesc = document.getElementById('task-desc').value.trim(); 
-  
-  if(taskDesc && !isNaN(taskDate.getDate())){
+  const taskDesc = document.getElementById('task-desc').value.trim();
+
+  if (taskDesc && !isNaN(taskDate.getDate())) {
     const calendarDays = document.getElementById('calendar').children;
-    for(let i=0; i<calendarDays.length; i++){
+
+    for (let i = 0; i < calendarDays.length; i++) {
       const day = calendarDays[i];
-      if(parseInt(day.textContent) === taskDate.getDate()){
+
+      if (parseInt(day.textContent) === taskDate.getDate()) {
         const taskElement = document.createElement('div');
+
         taskElement.className = 'task';
         taskElement.textContent = taskDesc;
 
         //contextmenu é evento de click com o lado direito do mouse
-        taskElement.addEventListener('contextmenu', function(event){
+        taskElement.addEventListener('contextmenu', function (event) {
           event.preventDefault();
           deleteTask(taskElement);
-
         })
 
-        taskElement.addEventListener('click', function(){
+        taskElement.addEventListener('click', function () {
           editTask(taskElement);
         })
+
         day.appendChild(taskElement);
         break;
       }
-    }    
+    }
     closeAddTaskModal();
   }
-  else{
+  else {
     alert('Please, enter a valid date and task description!');
   }
 }
 
-function selecionarPsico(psicologos){
+function selecionarPsico(psicologos) {
   console.log('Psicologos: ', psicologos);
-    psicologos.map(Element=>{
-      var option = document.createElement('option');
-      option.value = Element.idPsico
-      option.textContent = Element.nome;
-      document.getElementById('selecionePsico').append(option);
-    })
+
+  psicologos.map(Element => {
+    var option = document.createElement('option');
+    option.value = Element.idPsico
+    option.textContent = Element.nome;
+    document.getElementById('selecionePsico').append(option);
+  })
 }
-    
-
-
-   
-   
 
 
 
-   
+
+
+
+
+
+
