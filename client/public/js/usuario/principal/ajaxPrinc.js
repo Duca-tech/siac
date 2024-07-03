4748cb30fe5717c2cbef2f33f4719bb5efab5b6c
edugmenes <edugmenes1@hotmail.com>
2024-07-02 23:10:36 -0300

    Implemetação do link para o botão de histórico de consultas da tela principal

--- client/public/js/usuario/principal/ajaxPrinc.js
+++ client/public/js/usuario/principal/ajaxPrinc.js
@@ -1,15 +1,17 @@
+import { verificaoConsulta, verificarPerfil, detalheUser, tabelaHorarios } from '/js/usuario/principal/funcaoPrinc.js'
 
-
-import {verificaoConsulta, verificarPerfil, detalheUser, tabelaHorarios} from '/js/usuario/principal/funcaoPrinc.js'
 var idUser = localStorage.getItem('idUser');
 var token = localStorage.getItem('token');
 var perfil = localStorage.getItem('perfil');
-document.addEventListener('DOMContentLoaded', async function () {
+
+// O listener é criado para reagir aos clicks e interações do usuário com a tela:
+document.addEventListener('DOMContentLoaded', async () => {
+
     // Tela principal quando clica em agendar consulta:
-    var id ={
+    var id = {
         idUser: idUser
     }
-    console.log('id: ', idUser);
+    //console.log('id: ', idUser);
 
     try {
         const response = await fetch('/user/principal/ConsultasMarcadas', {
@@ -19,23 +21,24 @@ document.addEventListener('DOMContentLoaded', async function () {
             method: 'POST',
             body: JSON.stringify(id)
         });
+
         const data = await response.json();
-        console.log('Data: ', data);
+        //console.log('Data: ', data);
         await tabelaHorarios(data.results);
 
         var tabela = document.querySelector('.tableHorario tbody');
         if (tabela.children.length === data.results.length) {
             document.querySelectorAll('.btn-remarcar').forEach(button => {
-                button.addEventListener('click', function () {
+                button.addEventListener('click', () => {
                     document.querySelector('.modal').style.display = 'block';
                     const linha = this.closest('tr');
                     var idHorario = linha.getAttribute('idHorario');
                     remarcarConsulta(idHorario);
                 });
             });
-        
+
             document.querySelectorAll('.btn-cancelar').forEach(button => {
-                button.addEventListener('click', function () {
+                button.addEventListener('click', () => {
                     document.querySelector('.modal').style.display = 'block';
                     const linha = this.closest('tr');
                     var idHorario = linha.getAttribute('idHorario');
@@ -43,85 +46,81 @@ document.addEventListener('DOMContentLoaded', async function () {
                 });
             });
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error('error: ', error);
-    } finally {
+    }
+    finally {
         console.log('Requisição Finalizada!');
     }
-    
 
-    document.querySelector('.left-section .sidebar .dashboard').addEventListener('click', function(){
+
+    // Query selectors:
+    document.querySelector('.left-section .sidebar .dashboard').addEventListener('click', () => {
         window.location.href = '/user/dash';
     })
 
-    
-    document.querySelector('.main .quick-access .item').addEventListener('click', function () {
-        
-        
+    document.querySelector('.main .quick-access .item').addEventListener('click', () => {
     });
 
-    document.querySelector('.right-section .profile').addEventListener('click', function () {
+    document.querySelector('.right-section .profile').addEventListener('click', () => {
         window.location.href = '/user/principal/conta';
     });
 
-    document.querySelector('.left-section .sign-out').addEventListener('click', function () {
-            window.location.href = '/';
-        
+    document.querySelector('.left-section .sign-out').addEventListener('click', () => {
+        window.location.href = '/';
     });
 
-    document.getElementById('agendar-consulta').addEventListener('click', function(){
+    document.getElementById('historico-consultas').addEventListener('click', () => {
+        window.location.href = '/user/consultas/historico'
+    })
+
+    document.getElementById('agendar-consulta').addEventListener('click', () => {
         window.location.href = '/user/agendamento'
     })
 
-    document.getElementById('criar-agenda').addEventListener('click', function(){
-       window.location.href = '/psico/principal/agenda'
+    document.getElementById('criar-agenda').addEventListener('click', () => {
+        window.location.href = '/psico/principal/agenda'
     })
-    document.getElementById('prontuario').addEventListener('click', function(){
+    document.getElementById('prontuario').addEventListener('click', () => {
         window.location.href = '/psico/prontuario'
     })
 
-    $('#logout').on('click', function(e){
+    $('#logout').on('click', (e) => {
         e.preventDefault();
         window.location.href = '/'
     })
 
-    document.getElementById('verificarAgenda').addEventListener('click', function(){
+    document.getElementById('verificarAgenda').addEventListener('click', () => {
         window.location.href = '/recepcionista/principal/verificarConsulta'
     })
-
-    
-    
-
-
 });
 
 
-function remarcarConsulta(idHorario){
-    document.getElementById('confirmButton').addEventListener('click', function(){
+function remarcarConsulta(idHorario) {
+    document.getElementById('confirmButton').addEventListener('click', () => {
         window.location.href = '/user/agendamento/remarcacao?idHorario=' + idHorario
     })
 
-    document.getElementById('cancelButton').addEventListener('click', ()=>{
+    document.getElementById('cancelButton').addEventListener('click', () => {
         document.querySelector('.modal').style.display = 'none';
     })
-    
 }
 
-function cancelarConsulta(idHorario){
-    document.getElementById('confirmButton').addEventListener('click', function(){
+function cancelarConsulta(idHorario) {
+    document.getElementById('confirmButton').addEventListener('click', () => {
         console.log('Evento de cancelar consulta')
         fetch('/user/agendamento/cancelarConsulta?idHorario=' + idHorario)
-        .then(response=>response.json())
-        .then(data=>{
-            console.log('data: ', data)
-            alert(data.message);
-            location.reload();
-        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('data: ', data)
+                alert(data.message);
+                location.reload();
+            })
 
-        
     })
 
-    document.getElementById('cancelButton').addEventListener('click', ()=>{
+    document.getElementById('cancelButton').addEventListener('click', () => {
         document.querySelector('.modal').style.display = 'none';
     })
 }
