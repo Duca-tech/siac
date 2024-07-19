29ca57954578ba0491efbcc178b86a433bd94822
Wilson Ducatti <wilson.ducatti@obrasoft.com.br>
2024-07-18 21:52:52 -0300

    Atualizacao verificacao de perfil

--- server/config/verificacaoPerfil/verificacaoUser.js
+++ server/config/verificacaoPerfil/verificacaoUser.js
@@ -1,12 +1,23 @@
 var verificacaoUser = async (req,res, next) =>{
     var perfil = req.session.perfil;
     
-    if(perfil == 'psicologo' || perfil == 'administrador'){
+    if(perfil == 'paciente' || perfil == 'administrador'){
         next();
     }
     else{
-        res.render('/');
+        res.redirect('/');
     }
 }
 
-export { verificacaoUser}
+var verificacaoPsico = async (req,res, next)=>{
+    var perfil = req.session.perfil;
+
+    if(perfil == 'psicologo'|| perfil == 'administrador'){
+        next();
+    }
+    else{
+        res.redirect('/');
+    }
+}
+
+export { verificacaoUser,verificacaoPsico}
