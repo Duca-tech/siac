68c912fb33ec8fbecd38cae40e7975f5203654d5
Henrique <rique.debia12@gmail.com>
2024-08-24 11:28:38 -0300

    feat(home): update home with new card components

--- react-client/src/pages/Home.js
+++ react-client/src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import CardComponent from "./components/Card";
+import scheduleImage from "../images/1.webp";
 
 const Home = () => {
-  return <div>test</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <CardComponent
+        title="Agendar Consultas"
+        pathImage={scheduleImage}
+        redirectPath="/painel"
+      />
+      <CardComponent
+        title="Criar Agendas"
+        pathImage={scheduleImage}
+        redirectPath="/painel"
+      />
+      <CardComponent
+        title="Prontuários"
+        pathImage={scheduleImage}
+        redirectPath="/painel"
+      />
+    </div>
+  );
 };
 
 export default Home;
