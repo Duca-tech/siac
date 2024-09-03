4f6d8500347954935e75ddafc69b67ed10a8741f
edugmenes <edugmenes1@hotmail.com>
2024-09-03 20:40:33 -0300

    Feat(Login): Ajustes de front-end

--- react-client/src/pages/Login.js
+++ react-client/src/pages/Login.js
@@ -11,23 +11,29 @@ const Login = () => {
 
                         <div className="px-4 ms-xl-4">
                             <i className="fas fa-crosw fa-2x me-3 pt-5 mt-xl-4" style={{ color: '#709085' }}></i>
-                            <span className="h1 fw-semibold mb-0" style={{ fontSize: '5em' }}>SIAC</span>
+                            <p className="h1 fw-semibold mb-0" style={{ fontSize: '3.5em' }}>SIAC</p>
+                            <p className="h5 fw-light mt-2" style={{ fontSize: '1.1em' }}>Sistema Integrado de Atendimento às Clínicas</p>
                         </div>
 
                         <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 
                             <form style={{ width: '23rem' }}>
 
-                                <h3 className="fw-medium mb-2 pb-2" style={{ letterSpacing: '1px' }}>Log In</h3>
+                                <h3 className="fw-medium mb-2 pb-2" style={{ fontSize: '1.5em' }}>Log In</h3>
 
                                 <div data-mdb-input-init className="form-outline mb-2">
                                     <input type="email" id="form2Example18" className="form-control form-control-lg" />
-                                    <label className="form-label" for="form2Example18">Endereço de email</label>
+                                    <label className="form-label"
+                                        for="form2Example18"
+                                        style={{ fontSize: '0.95em' }}>Endereço de email
+                                    </label>
                                 </div>
 
                                 <div data-mdb-input-init className="form-outline mb-4">
                                     <input type="password" id="form2Example28" className="form-control form-control-lg" />
-                                    <label className="form-label" for="form2Example28">Senha</label>
+                                    <label className="form-label"
+                                        for="form2Example28"
+                                        style={{ fontSize: '0.95em' }}>Senha</label>
                                 </div>
 
                                 <div className="pt-1 mb-3">
@@ -35,10 +41,10 @@ const Login = () => {
                                         className="btn btn-info btn-lg btn-block"
                                         type="button"
                                         style={{ backgroundColor: '#1a73e8', color: 'white', borderColor: 'white' }}
-                                    >Login</button>
+                                    >Log In</button>
                                 </div>
 
-                                <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Esqueceu a senha?</a></p>
+                                <p className="small mb-4 pb-lg-2"><a className="text-muted" href="#!">Esqueceu a senha?</a></p>
                                 <p>Ainda não tem conta? <a href="#!" className="link-info">Cadastre-se aqui</a></p>
 
                             </form>
